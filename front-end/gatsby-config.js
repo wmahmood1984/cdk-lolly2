bf068ff142e17d9dba32c5ca7de69b4ffbe291fa
Waqas Mahmood <waqasnizamani3@gmail.com>
2020-12-27 08:07:06 +0500

    garphql key added again

--- front-end/gatsby-config.js
+++ front-end/gatsby-config.js
@@ -22,9 +22,9 @@ module.exports = {
         // Field under which the remote schema will be accessible. You'll use this in your Gatsby query
         fieldName: "swapi",
         // Url to query from
-        url: `https://fbbmocusy5dgng6elseajuggt4.appsync-api.eu-west-1.amazonaws.com/graphql`,
+        url: `https://3ibkqi365jfzvivalopoblzsuq.appsync-api.eu-west-1.amazonaws.com/graphql`,
         headers: {
-          "x-api-key": "da2-nuo2pxnjdjf2pdm3krclbqbd3i", // ENTER YOUR API KEY HERE
+          "x-api-key": "da2-4e5hpgtowjah5hgajyfh2e7cju", // ENTER YOUR API KEY HERE
         },
 
       },
--- front-end/src/apollo/client.js
+++ front-end/src/apollo/client.js
@@ -3,10 +3,10 @@ import { ApolloClient, InMemoryCache,HttpLink } from '@apollo/client';
 
 export const client = new ApolloClient({
     link: new HttpLink({
-        uri: 'https://fbbmocusy5dgng6elseajuggt4.appsync-api.eu-west-1.amazonaws.com/graphql',
+        uri: 'https://3ibkqi365jfzvivalopoblzsuq.appsync-api.eu-west-1.amazonaws.com/graphql',
        fetch, 
        headers: {
-        "x-api-key": "da2-nuo2pxnjdjf2pdm3krclbqbd3i", // ENTER YOUR API KEY HERE
+        "x-api-key": "da2-4e5hpgtowjah5hgajyfh2e7cju", // ENTER YOUR API KEY HERE
       },
     }),
     cache: new InMemoryCache()
